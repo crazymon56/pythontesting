e7a02a0864953bbbed21c06414d2e74f153d9c64
crazymon56 <rctj12@gmail.com>
2018-07-16 17:54:18 -0500

    atryatsomething

--- site.py
+++ site.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis, RedisError
 import os
 import socket
-
+import codecs
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
@@ -10,16 +10,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-
-    html = "<h1><center>Happy Times</center></h1>" \
-	   "<h1><center>Login</center></h1>" \
-	   "<form>" \
-	   "<h3><center>Username</center></h3>" \
-	   "<center><input type=text name=Username value=JohnDoe></center><br>" \
-	   "<h3><center>Password</center></h3>" \
-	   "<center><input type=text name=Password value=JohnDoe1></center><br>" \
-	   "<center><input type=submit class=submit value=Login></center>" \
-	   "</form>"   
+	f = codecs.open("looks.html", "r")
+      
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
