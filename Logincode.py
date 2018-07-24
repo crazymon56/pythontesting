2be632e227579dc2d1cbb60f28e8d9813fa234bf
crazymon56 <rctj12@gmail.com>
2018-07-23 19:57:07 -0500

    home

--- Logincode.py
+++ Logincode.py
@@ -10,32 +10,36 @@ redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = Flask(__name__)
 
 app.secret_key = 'k4k29_dk!!ko'
-'''
-for n in Ndatabase:
-            if n == UserN:
-                for p in Pdatabase:
-                    if n == UserP:
-'''
+
 #Signing Up
 @app.route("/SignUp", methods=['POST', 'GET'])
 def signup():
-    Userinfo = [request.form['SUsername'] + ',' + request.form['SPassword'], ]
-    UserN = request.form['SUsername']
     UserP = request.form['SPassword']
     UserCP = request.form['SCPassword']
-    if UserP != USerCP:
+    if UserP != UserCP:
         with open('Invalidlogin.html', 'r') as fh:        
             html = fh.read()
         return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+    with open('Signupcon.html', 'r') as fh:        
+        html = fh.read()
+        return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+#Loginpage
+@app.route("/Login", methods=['POST', 'GET'])
+def login():
+    with open('Login.html', 'r') as fh:
+        html = fh.read()
+        
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
 #LoginCheck        
-@app.route("/Login", methods=['POST', 'GET'])    
-def login(): 
+@app.route("/Logged", methods=['POST', 'GET'])    
+def logged(): 
     Userinfo = [request.form['SUsername'] + ',' + request.form['SPassword'], ]
-    UserN = request.form['SUsername']
-    UserP = request.form['SPassword']
-    UserCP = request.form['SCPassword']
+    UserN = request.form['LUsername']
+    UserP = request.form['LSPassword']
     usercheck = False
-       
+    return request.form['SUsername']   
     if request.method == 'POST':
         session['username'] = UserN
         for x  in Userinfo:
@@ -50,6 +54,7 @@ def login():
             with open('Invalidlogin.html', 'r') as fh:        
                 html = fh.read()        
             return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())                    
+
 # Login
 @app.route("/", methods=['GET', 'POST'])
 def index():
