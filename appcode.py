54a821bc51178d5d4a2fd1f9dcb39b81aa69750a
crazymon56 <rctj12@gmail.com>
2018-08-28 18:30:27 -0500

    goodprogress

--- appcode.py
+++ appcode.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, url_for, session, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
 import socket
 import codecs
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "slwol;ayesal."
 socketio = SocketIO(app)
 
-UserIn = mysql.connector.connect(user="root", db="userinformation", passwd="passwordfun", host="mysqldb")
+UserIn = mysql.connector.connect(user="root", db="chatinformation", passwd="passwordfun", host="mysqldb")
 
 
 
@@ -36,6 +36,17 @@ def handle_message():
 def message_handle(message):
     emit('usermessage', {'data': message['data']})
 
+@socketio.on('userjoin', namespace='/test')
+def join_handle(sentroom):
+    cursor = UserIn.cursor()
+    username = session['username']
+    join_room(sentroom['data'])
+    cursor.execute("INSERT INTO chatname FROM chats VALUES(%s)", (sentroom['data']))
+    cursor.execute("INSERT INTO channelname, linkid FROM channels VALUES('general', (SELECT id FROM chats WHERE chatname=%s))", (sentroom['data']))
+    cursor.execute("INSERT INTO message, outid FROM messages VALUES(%s, (SELECT ))")
+    emit('confirmjoin', {'data' : session['username'] + ' has joined'})
+
+
 #SigningUp
 @app.route("/SignUpCon", methods=['POST', 'GET'])
 def signup():
@@ -45,7 +56,7 @@ def signup():
             html = fh.read()
         return html
     
-    cursor.execute("INSERT INTO user (username, password, confirmpassword) VALUES(%s, %s, %s);", (request.form["SUsername"], request.form["SPassword"], request.form["SCPassword"]))
+    cursor.execute("INSERT INTO users (username, password, confirmpassword) VALUES(%s, %s, %s);", (request.form["SUsername"], request.form["SPassword"], request.form["SCPassword"]))
     UserIn.commit()
     cursor.close()
 
@@ -74,24 +85,26 @@ def logged():
     cursor = UserIn.cursor()
     if request.method == 'POST':
         session['username'] = request.form['LUsername']
-        validlog = False 
-        cursor.execute("SELECT username, password FROM user label: LOOP IF user[0] == %s AND user[1] == %s THEN SET %s = %s END IF; LEAVE label; END LOOP label;", (request.form['LUsername'], request.form['LPassword'], validlog, True))
+        validlog = 0 
+        cursor.execute("SELECT username, password FROM users")
+        
+        #cursor.execute("label1: LOOP IF users[0] == %s AND users[1] == %s THEN SET %s = %s; ELSE ITERATE label1; END IF; LEAVE label1; END LOOP label1;", (request.form['LUsername'], request.form['LPassword'], validlog, True))
         
 
-        # for items in cursor:
-        #     if(items[0] == request.form['LUsername'] and items[1] == request.form['LPassword']):
-        #         validlog = True
+        for items in cursor:
+            if(items[0] == request.form['LUsername'] and items[1] == request.form['LPassword']):
+                validlog = True
 
-        cursor.close()
-        print(validlog)        
-        if(validlog == True):
+        if(validlog == 1):  
+            cursor.close()
             return redirect('BlahChat')
                 
         else:        
             with open('Invalidlogin.html', 'r') as fh:        
                 html = fh.read() 
                 cursor.close()       
-            return html                   
+            return html    
+                           
 
 #SignUp
 @app.route("/", methods=['GET', 'POST'])
